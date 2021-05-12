72e4727959baa34b944f42039933ec4c0b5b11b6
Bardia Biramzadeh <bardia-bb@gmx.de>
2021-05-12 17:53:09 +0200

    createdSignupSignInComponents

---
+++ src/components/SignIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SignIn(props){
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="InputEmail">Username</label>
+                <input type="email" className="form-control" id="InputEmail" name="email" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="InputPassword">Password</label>
+                <input name="password" type="password" className="form-control" id="InputPassword" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    )
+}
+
+export default SignIn
